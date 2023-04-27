636b275a78e06dccdb9640cd37cf8931d915e79b
EgemenBaykan <30087171+EgemenBaykan@users.noreply.github.com>
2023-04-27 21:52:43 +0300

    Add files via upload
    Class structure implemented.

--- FilterGen.py
+++ FilterGen.py
@@ -1,9 +1,6 @@
-import sys
-import getopt
 import pandas as pd
 import numpy as np
 import math
-import matplotlib.pyplot as plt
 
 def to_GHz(freq):
     return freq*1e9
@@ -20,111 +17,83 @@ def to_pF(cap):
 def to_nF(cap):
     return cap*1e9 
 
-def solveBPF(filter_characteristic, fcut_low, fcut_high, impedance, filter_order):
-    filter_coef = pd.read_excel('Filter_Coefficients.xlsx', sheet_name=filter_characteristic)
-    f_res = math.sqrt(fcut_low * fcut_high)
-    coeffs = filter_coef.iloc[filter_order-1].values
-    coeffs = coeffs[~np.isnan(coeffs)]
+class Filter:
+    def __init__(self, filter_type, filter_order, filter_characteristic, impedance):
+        self.filter_type = filter_type
+        self.filter_characteristic = filter_characteristic
+        self.filter_order = filter_order
+        self.impedance = impedance
+        self.fcut = 0
+        self.fcut_low = 0
+        self.fcut_high = 0
 
-    C = np.zeros(10)
-    L = np.zeros(10)
+        filter_coef = pd.read_excel('Filter_Coefficients.xlsx', sheet_name=self.filter_characteristic)
+        self.coeffs = filter_coef.iloc[filter_order-1].values
+        self.coeffs = self.coeffs[~np.isnan(self.coeffs)]
 
-    print(coeffs)
-
-    for i in range(1,len(coeffs)):
-        coeff = coeffs[i]
-        if(i%2==1):
-            C[i-1] = coeff / (2*math.pi*(fcut_high-fcut_low)*impedance)
-            L[i-1] = ((fcut_high-fcut_low)*impedance) / (2*math.pi*fcut_low*fcut_high*coeff)
-        elif(i%2==0):
-            C[i-1] = (fcut_high - fcut_low) / (2*math.pi*fcut_high*fcut_low*impedance*coeff)
-            L[i-1] = (impedance*coeff) / (2 * math.pi * (fcut_high-fcut_low))
-
-    C = C[C != 0]
-    L = L[L != 0]
-
-    print("Values for C")
-    print(C)
-    print("Values for L")
-    print(L)
+        self.C = np.zeros(10)
+        self.L = np.zeros(10)
 
 
-def solveLPF(filter_characteristic, fcut, impedance, filter_order):
-    filter_coef = pd.read_excel('Filter_Coefficients.xlsx', sheet_name=filter_characteristic)
+    def __str__(self):
+        pass
 
-    coeffs = filter_coef.iloc[filter_order-1].values
-    coeffs = coeffs[~np.isnan(coeffs)]
+    def solve(self):
+        if(self.filter_type == 'LPF'):
+            for i in range(1, len(self.coeffs)):
+                coeff = self.coeffs[i]
+                self.C[i-1] = coeff / (2*math.pi*self.fcut*self.impedance)
+                self.L[i-1] = (self.impedance * coeff) / (2*math.pi*self.fcut)
+        elif(self.filter_type == 'HPF'):
+            for i in range(1, len(self.coeffs)):
+                coeff = self.coeffs[i]
+                self.L[i-1] = (self.impedance) / (2*math.pi*self.fcut*coeff)
+                self.C[i-1] = 1/ (2*math.pi*self.fcut*self.impedance*coeff)
+        elif(self.filter_type == 'BPF'):
+            for i in range(1,len(self.coeffs)):
+                coeff = self.coeffs[i]
+                if(i%2==1):
+                    self.C[i-1] = coeff / (2*math.pi*(self.fcut_high-self.fcut_low)*self.impedance)
+                    self.L[i-1] = ((self.fcut_high-self.fcut_low)*self.impedance) / (2*math.pi*self.fcut_low*self.fcut_high*coeff)
+                elif(i%2==0):
+                    self.C[i-1] = (self.fcut_high - self.fcut_low) / (2*math.pi*self.fcut_high*self.fcut_low*self.impedance*coeff)
+                    self.L[i-1] = (self.impedance*coeff) / (2 * math.pi * (self.fcut_high-self.fcut_low))
+        elif(self.filter_type == 'BSF'):
+            print('Band stop filters are not implemented yet. Exiting the program.')
+            exit()
+        else:
+            print('Invalid filter type. Exiting the program')
+            exit()
 
-    C = np.zeros(10)
-    L = np.zeros(10)
+        self.C = self.C[self.C != 0]
+        self.L = self.L[self.L != 0]
 
-    for i in range(1, len(coeffs)):
-        coeff = coeffs[i]
-        C[i-1] = coeff / (2*math.pi*fcut*impedance)
-        L[i-1] = (impedance * coeff) / (2*math.pi*fcut)
-
-    C = C[C != 0]
-    L = L[L != 0]
-    
-    print("Values for C")
-    print(C)
-    print("Values for L")
-    print(L)
-
-
-def solveHPF(filter_characteristic, fcut, impedance, filter_order):
-    filter_coef = pd.read_excel('Filter_Coefficients.xlsx', sheet_name=filter_characteristic)
-
-    coeffs = filter_coef.iloc[filter_order-1].values
-    coeffs = coeffs[~np.isnan(coeffs)]
-
-    C = np.zeros(10)
-    L = np.zeros(10)
-
-    for i in range(1, len(coeffs)):
-        coeff = coeffs[i]
-        L[i-1] = (impedance) / (2*math.pi*fcut*coeff)
-        C[i-1] = 1/ (2*math.pi*fcut*impedance*coeff)
-
-    C = C[C != 0]
-    L = L[L != 0]
-    
-    print("Values for C")
-    print(C)
-    print("Values for L")
-    print(L)
-
-# TODO: WILL BE IMPLEMENTED
-def solveBSF():
-    pass
-
+        return self.C, self.L
 
 def main():
 
     filter_type = input('Filter Type: ')
-    if(filter_type == 'BPF' or filter_type == 'BSF'):
-        fcut_low = float(input('Low cutoff frequency: '))
-        fcut_high = float(input('High cutoff frequency: '))
-    elif(filter_type == 'LPF' or filter_type == 'HPF'):
-        fcut = float(input('Cutoff frequency: '))
-    else:
-        print('Invalid input for filter type. Exiting the program')
-        exit()
 
     filter_characteristic = input('Filter characteristic: ')
     impedance = float(input('Impedance: '))
     filter_order = int(input('Filter order: '))
 
-    if(filter_type == 'BPF'):
-        solveBPF(filter_characteristic, fcut_low, fcut_high, impedance, filter_order)
-    elif(filter_type == 'LPF'):
-        solveLPF(filter_characteristic, fcut, impedance, filter_order)
-    elif(filter_type == 'HPF'):
-        solveHPF(filter_characteristic, fcut, impedance, filter_order)
-    elif(filter_type == 'BSF'):
-        solveBSF(filter_characteristic, fcut_low, fcut_high, impedance, filter_order)
+    flt = Filter(filter_type, filter_order, filter_characteristic, impedance)
+    
+    if(filter_type == 'BPF' or filter_type == 'BSF'):
+        flt.fcut_low = float(input('Low cutoff frequency: '))
+        flt.fcut_high = float(input('High cutoff frequency: '))
+    elif(filter_type == 'LPF' or filter_type == 'HPF'):
+        flt.fcut = float(input('Cutoff frequency: '))
     else:
-        print('Invalid input for filter type. Exiting the program')
+        print('Invalid input for given filter type. Exiting the program.')
         exit()
+
+    C, L = flt.solve()
+
+    print("Values for C")
+    print(C)
+    print("Values for L")
+    print(L)
         
 main()
